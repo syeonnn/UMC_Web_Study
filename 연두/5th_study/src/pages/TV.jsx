5ba19d7817824a1aace87f88e0bec84ad4f5dc91
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2023-12-18 15:07:44 +0900

    Delete 연두 directory

--- 연두/5th_study/src/pages/TV.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function TV() {
-  return <div>TV</div>;
-}
--- 연두/7th_study/src/components/Ad.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ReactComponent as Banner } from "../img/image.svg";
-
-export default function Ad(props) {
-  return <>{props.show && <Banner width="100%" />}</>;
-}
